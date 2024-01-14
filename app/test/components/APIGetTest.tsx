8a96c7ff738e3a0dc8d8029db582695595e379ee
Connor Wales <connorwales@gmail.com>
2024-01-13 21:22:02 -0800

    reorganize folders for api routes and test components

---
+++ app/test/components/APIGetTest.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { useEffect, useState } from "react";
+
+export default function APIGetTest() {
+
+  const [data, setData] = useState("")
+
+  useEffect(() => {
+    fetch('/api/sampleget').then((res) => {
+      console.log("expecting results....");
+      res.json().then(json => {
+        console.log(json.data);
+        setData(json.data);
+      });
+    })
+  }, [])
+
+
+  return (
+    <h1>
+      API Get Test {data}
+    </h1>
+  )
+}
---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+import APIGetTest from "./components/APIGetTest";
+
+export default async function TestPage() {
+  return (
+    <>
+      <APIGetTest />
+    </>
+  )
+}
