873d4238303fa0ba04b1effe260196efa8f9007d
Connor Wales <connorwales@gmail.com>
2024-01-21 22:04:09 -0800

    fix PusherTest component to remove subscription and bind from useEffect

--- app/api/samplePusher/route.tsx
+++ app/api/samplePusher/route.tsx
@@ -1,17 +1,15 @@
 import Pusher from "pusher";
 
+const pusher = new Pusher({
+  appId: process.env.PUSHER_APP_ID as string,
+  key: process.env.PUSHER_KEY as string,
+  secret: process.env.PUSHER_SECRET as string,
+  cluster: process.env.PUSHER_CLUSTER as string,
+  useTLS: true,
+});
 
 export async function POST(req: Request) {
 
-  const pusher = new Pusher({
-    appId: process.env.PUSHER_APP_ID as string,
-    key: process.env.PUSHER_KEY as string,
-    secret: process.env.PUSHER_SECRET as string,
-    cluster: process.env.PUSHER_CLUSTER as string,
-    useTLS: true,
-  });
-
-  
   const { message, sender } = await req.json();
 
   // const json = await req.json();
--- app/test/components/PusherTest.tsx
+++ app/test/components/PusherTest.tsx
@@ -8,7 +8,18 @@ export default function PusherTest() {
 
 
   // Enable pusher logging - don't include this in production
-  // Pusher.logToConsole = true;
+  Pusher.logToConsole = true;
+
+  const pusher = new Pusher("a1bb6b25bb513f4d5a0a", {
+    cluster: "us3"
+  });
+
+  const channel = pusher.subscribe('my-channel');
+  channel.bind('my-event', function(data: any) {
+    const stringified = JSON.stringify(data);
+    setPusherData(stringified);
+    console.log(stringified);
+  });
 
   useEffect(() => {
 
@@ -21,23 +32,6 @@ export default function PusherTest() {
     })
     .then(res => res.json())
     .then(json => console.log(json));
-
-    console.log(process.env.PUSHER_KEY, process.env.PUSHER_CLUSTER)
-
-    // const pusher = new Pusher(process.env.PUSHER_KEY as string, {
-    //   cluster: process.env.PUSHER_CLUSTER as string
-    // });
-
-    const pusher = new Pusher("a1bb6b25bb513f4d5a0a", {
-      cluster: "us3"
-    });
-  
-    const channel = pusher.subscribe('my-channel');
-    channel.bind('my-event', function(data: any) {
-      const stringified = JSON.stringify(data);
-      setPusherData(stringified);
-      console.log(stringified);
-    });
 
     return (() => {
       pusher.unsubscribe('my-channel');
