b5902462003cc4856db5b5535ddcd7d8fd7aec20
Connor Wales <connorwales@gmail.com>
2024-01-22 22:28:23 -0800

    install next-auth, add api/auth/[...nextauth]/route.ts, begin addition of credentials

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,37 @@
+import NextAuth, { NextAuthOptions } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+
+const options: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Crednetials",
+      credentials: {
+        username: {
+          label: "Username",
+          type: "text",
+          placeholder: "username"  
+        },
+        password: {
+          label: "password",
+          type: "password"
+        }
+      },
+      async authorize(credentials, request) {
+        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      }
+    })
+  ]
+}
+
+export default NextAuth(options);
