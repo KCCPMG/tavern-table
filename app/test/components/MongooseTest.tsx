d2d628efcd8fa2e3878664b68e893f1571b2417d
Connor Wales <connorwales@gmail.com>
2024-02-18 11:52:43 -0800

    add models/Campaign, refactor MongooseTest to retrieve Campaigns as well

--- app/test/components/MongooseTest.tsx
+++ app/test/components/MongooseTest.tsx
@@ -1,16 +1,27 @@
 "use server"
 import mongooseConnect from "@/lib/mongooseConnect";
 import User from "@/models/User";
+import Campaign from "@/models/Campaign";
 
 export default async function MongooseTest() {
 
   await mongooseConnect();
+
   const users = await User.find({});;
+  const campaigns = await Campaign.find({});
   
 
   return (
-    <h1>
-      Mongoose Test: {JSON.stringify(users)}
-    </h1>
+    <>
+      <br />
+      <h2 className="text-2xl font-bold">
+        Mongoose Test:  
+      </h2> 
+      <br />
+      Users: {JSON.stringify(users)}
+      <br />
+      Campaigns: {JSON.stringify(campaigns)};
+
+    </>
   )
 }
---
+++ models/Campaign.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+const Campaign = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  createdBy: {
+    type: mongoose.Types.ObjectId,
+    required: true
+  },
+  createdOn: {
+    type: Date,
+    default: new Date(),
+    required: true
+  },
+  description: {
+    type: String,
+    required: false
+  },
+  dm: {
+    type: [mongoose.Types.ObjectId]
+  },
+  handouts: {
+    type: [mongoose.Types.ObjectId]
+  },
+  game: {
+    type: String,
+    required: false
+  },
+  players: {
+    type: [mongoose.Types.ObjectId]
+  },
+  invitedPlayers: {
+    type: [mongoose.Types.ObjectId],
+    default: []
+  },
+  journalEntries: {
+    type: [mongoose.Types.ObjectId],
+    default: []
+  },
+  index: {
+    type: [mongoose.Types.ObjectId],
+    default: []
+  },
+  threadId: {
+    type: mongoose.Types.ObjectId,
+    required: true
+  }
+})
+
+export default mongoose.models.Campaign || mongoose.model('Campaign', Campaign);
+
