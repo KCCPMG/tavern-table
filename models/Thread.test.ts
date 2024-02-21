23850bd69d1fc92516c1a90372abbcd66adfda27
Connor Wales <connorwales@gmail.com>
2024-02-20 22:52:15 -0800

    complete basic tests on Thread.test.ts

--- models/Thread.test.ts
+++ models/Thread.test.ts
@@ -13,3 +13,77 @@ type RequiredThreadValues = {
   chatType: ChatTypes,
   name: string
 }
+
+const newChatThreadDetails = {
+  name: "test",
+  chatType: CHAT
+}
+
+const newRoomThreadDetails = {
+  name: "test",
+  chatType: ROOM
+}
+
+const newCampaignThreadDetails = {
+  name: "test",
+  chatType: CAMPAIGN
+}
+
+
+beforeAll(async function() {
+  await mongooseConnect();
+  await Thread.deleteMany({name: "test"})
+})
+
+afterAll(async function() {
+  await mongoose.disconnect();
+})
+
+
+describe("A thread", function() {
+
+  test("can be created", async function() {
+
+    // make returned ThreadType obj indexable by string
+    const [newChatThread, newRoomThread, newCampaignThread] = await Promise.all([
+      Thread.create(newChatThreadDetails),
+      Thread.create(newRoomThreadDetails),
+      Thread.create(newCampaignThreadDetails)
+    ])
+
+    for (let [key, val] of Object.entries(newChatThreadDetails)) {
+      expect(newChatThread).toHaveProperty(key);
+      expect(newChatThread[key]).toBe(val);
+    }
+
+    for (let [key, val] of Object.entries(newRoomThreadDetails)) {
+      expect(newRoomThread).toHaveProperty(key);
+      expect(newRoomThread[key]).toBe(val);
+    }
+
+    for (let [key, val] of Object.entries(newCampaignThreadDetails)) {
+      expect(newCampaignThread).toHaveProperty(key);
+      expect(newCampaignThread[key]).toBe(val);
+    }
+
+  })
+
+  test("can be retrieved", async function() {
+    const foundThreads: {[index: string]: ThreadType}[] = await Thread.find({name: "test"});
+    expect(foundThreads.length).toBe(3);
+    foundThreads.forEach(ft => {
+      expect(ft).toHaveProperty('name');
+      expect(ft.name).toBe('test');
+    })
+  })
+
+  test("can be deleted", async function() {
+    await Thread.deleteMany({name: "test"});
+  })
+
+  test("will not be retrieved", async function() {
+    const foundThreads: {[index: string]: ThreadType} [] = await Thread.find({name: "test"});
+    expect(foundThreads.length).toBe(0);
+  })
+
+})
