07ffa13c8cf0b6263e7810051eb1dd8a27ce980a
Connor Wales <connorwales@gmail.com>
2024-05-27 15:55:41 -0700

    modify api/campaigns/route POST to return all campaigns for a user, refactor client side code in components/Campaigns to put AddCampaignButton and AddCampaignModal fully within Campaigns to avoid creating context or further prop drilling

--- app/api/campaigns/route.tsx
+++ app/api/campaigns/route.tsx
@@ -3,11 +3,8 @@ import User from "@/models/User";
 import { NextRequest, NextResponse } from "next/server";
 
 import { getServerSession } from "next-auth";
-import { getSession } from "next-auth/react";
 import { authOptions } from "@/api/auth/[...nextauth]/route";
-import { getToken } from "next-auth/jwt";
 
-import { signIn } from "next-auth/react";
 import mongooseConnect from '@/lib/mongooseConnect';
 import { UserNotFoundErr } from "@/lib/NextError";
 
@@ -17,17 +14,9 @@ import { UserNotFoundErr } from "@/lib/NextError";
 export async function GET(req: NextRequest, res: NextResponse) {
 
   try {
-    // const session = await getSession();
-    // console.log({"session on route": session});
 
     await mongooseConnect();
     const serverSession = await getServerSession(authOptions);
-    // console.log({"server session on route": serverSession});
-  
-    // console.log({req});
-    // console.log({cookies: req.cookies})
-    // const token = await getToken({ req });
-    // console.log({token});
 
     if (!(serverSession?.user)) throw UserNotFoundErr;
 
@@ -37,17 +26,6 @@ export async function GET(req: NextRequest, res: NextResponse) {
     const campaigns = await user.getCampaigns();
     return Response.json(campaigns);
 
-    // const campaignIds = serverSession?.user.campaigns;
-
-    // const campaignPromises = campaignIds?.map(cid => {
-    //   return Campaign.findById(cid);
-    // }) || [];
-
-    // console.log(campaignPromises);
-  
-    // // const campaigns = await Promise.all(campaignPromises);
-
-    // return Response.json(campaigns);
 
   } catch(err) {
     return Response.error();
@@ -62,17 +40,20 @@ export async function POST(req: NextRequest, res: NextResponse) {
 
     const serverSession = await getServerSession(authOptions);
 
+    if (!(serverSession?.user)) throw UserNotFoundErr;
+
     const { createObj } = await req.json();
-    console.log({createObj});
-    const campaign = await Campaign.createCampaign({
+    // console.log({createObj});
+    await Campaign.createCampaign({
       ...createObj,
       creatorId: serverSession?.user._id
     });
-    console.log({campaign});
-
     
-
-    return Response.json(campaign);
+    // refresh user
+    const user = await User.findById(serverSession.user._id);
+    if (!user) throw UserNotFoundErr;
+    const campaigns = await user.getCampaigns();
+    return Response.json(campaigns);
 
   } catch(err) {
     console.log(err);
--- components/Campaigns.tsx
+++ components/Campaigns.tsx
@@ -8,84 +8,6 @@ import { ICampaign } from "@/models/Campaign";
 import Link from "next/link";
 
 
-export function AddCampaignModal() {
-
-  const { setShowModal } = useModalContext();
-  const [campaignName, setCampaignName] = useState("");
-  const [game, setGame] = useState("");
-
-  async function submitForm(e: FormEvent): Promise<void> {
-    e.preventDefault();
-    console.log("ya clicked me");
-    const req = await fetch('/api/campaigns', {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        createObj: {
-          game,
-          name: campaignName
-        }
-      })
-    })
-    setShowModal(false);
-    setCampaignName("");
-    setGame("");
-    const json = await req.json();
-    // console.log(json);
-
-  }
-
-  return (
-    <div>
-      <h2>Create New Campaign</h2>
-      <hr />
-      <form onSubmit={submitForm}>
-        <label className="block">
-          Campaign Name:
-          <input 
-            type="text" required 
-            className="block"
-            value={campaignName}
-            onChange={(e) => setCampaignName(e.target.value)} 
-          />
-        </label>
-        <label className="block">
-          Game:
-          <input 
-            className="block"
-            type="text" required
-            value={game}
-            onChange={(e) => setGame(e.target.value)}
-          />
-        </label>
-        <button type="submit">Create Campaign!</button>
-      </form>
-
-    </div>
-  )
-}
-
-export function AddCampaignButton() {
-
-  const { showModal, setShowModal, setModalBody } = useModalContext();
-  const [displayModal, setDisplayModal] = useState(false); 
-
-  return (
-    <>
-      <button onClick={() => { 
-        // console.log(displayModal);
-        setModalBody(<AddCampaignModal />)
-        setShowModal(true); 
-      }}>
-        Add Campaign
-      </button>
-    </>
-  )
-}
-
-
 type CampaignLinkProps = {
   name: string,
   id: string
@@ -112,6 +34,79 @@ export default function Campaigns({initialCampaigns}: CampaignsProps) {
   const [campaigns, setCampaigns] = useState<Array<ICampaign>>(initialCampaigns || []);
 
   console.log(campaigns);
+
+  function AddCampaignModal() {
+    const { setShowModal } = useModalContext();
+    const [campaignName, setCampaignName] = useState("");
+    const [game, setGame] = useState("");
+  
+    async function submitForm(e: FormEvent): Promise<void> {
+      e.preventDefault();
+      console.log("ya clicked me");
+      const req = await fetch('/api/campaigns', {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          createObj: {
+            game,
+            name: campaignName
+          }
+        })
+      })
+      setShowModal(false);
+      setCampaignName("");
+      setGame("");
+      const campaigns = await req.json();
+      console.log(campaigns);
+      setCampaigns(campaigns);
+    }
+  
+    return (
+      <div>
+        <h2>Create New Campaign</h2>
+        <hr />
+        <form onSubmit={submitForm}>
+          <label className="block">
+            Campaign Name:
+            <input 
+              type="text" required 
+              className="block"
+              value={campaignName}
+              onChange={(e) => setCampaignName(e.target.value)} 
+            />
+          </label>
+          <label className="block">
+            Game:
+            <input 
+              className="block"
+              type="text" required
+              value={game}
+              onChange={(e) => setGame(e.target.value)}
+            />
+          </label>
+          <button type="submit">Create Campaign!</button>
+        </form>
+  
+      </div>
+    )
+  }
+
+  function AddCampaignButton() {
+    const { setShowModal, setModalBody } = useModalContext();
+  
+    return (
+      <>
+        <button onClick={() => { 
+          setModalBody(<AddCampaignModal />)
+          setShowModal(true); 
+        }}>
+          Add Campaign
+        </button>
+      </>
+    )
+  }
 
   return (
     <>
