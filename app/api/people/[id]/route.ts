aa097f254e5e429889d334ea4479daed2d9c371e
Connor Wales <connorwales@gmail.com>
2024-06-11 09:20:49 -0700

    refactor api/people/[id]/route to catch and handle NextErrors

--- app/api/people/[id]/route.ts
+++ app/api/people/[id]/route.ts
@@ -1,13 +1,23 @@
-import { UserNotFoundErr } from "@/lib/NextError";
+import { NextError, UserNotFoundErr } from "@/lib/NextError";
+import mongooseConnect from "@/lib/mongooseConnect";
 import User, { IPerson } from "models/User";
 import { NextRequest } from "next/server";
 
 export async function GET(req: NextRequest, {params} : {params: {id: string}}) {
   try {
-    const person = await User.getPerson(params.id);
-    if (!person) throw UserNotFoundErr;
+    const person = await User.getPerson(params.id); // throws UserNotFoundErr if no user
     return Response.json(person);
   } catch(err) {
-    return Response.error();
+
+    // console.log(err instanceof UserNotFoundErr);
+
+    if (err?.constructor.name === "NextError") {
+      const error = err as NextError;
+      return new Response(error.message, {
+        status: error.status,
+        statusText: error.message
+      })
+    } else return Response.error();
+
   }
 }
