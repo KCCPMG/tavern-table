22020fbaf18204209e49828ed524e59304ec2660
Connor Wales <connorwales@gmail.com>
2024-06-10 18:51:41 -0700

    partial refactor of models/Campaign, create InviteMessageModal, does not yet have API connection

--- components/Campaign.tsx
+++ components/Campaign.tsx
@@ -6,6 +6,129 @@ import { useModalContext } from "context/ModalContext";
 import { IPerson } from "@/models/User";
 
 
+type InviteMessageModalProps = {
+  player: IPerson,
+  campaignId: string,
+  campaignName: string
+}
+
+function InviteMessageModal(
+  { player, campaignId, campaignName }: InviteMessageModalProps) 
+{
+  
+  const { setShowModal, setModalBody } = useModalContext();
+  const [messageText, setMessageText] = useState("");
+
+  const handleTextChange: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
+    setMessageText(e.target.value);
+  }
+
+  const cancelInvitation = () => {
+    setModalBody(
+      <InvitePlayersModal 
+        campaignName={campaignName} 
+        campaignId={campaignId}
+      />
+    )
+  }
+
+  const sendInvitation = () => {
+    // placeholder action
+  }
+
+  return (
+    <form onSubmit={sendInvitation}>
+      <h4>Invite {player.username} to {campaignName}</h4>
+      <textarea rows={4} onChange={handleTextChange}></textarea>
+      <div>
+        <button onClick={cancelInvitation}>Cancel</button>
+        <button>Invite!</button>
+      </div>
+    </form>
+  )
+}
+
+
+type InvitePlayersModalProps = {
+  campaignId: string,
+  campaignName: string
+}
+
+
+function InvitePlayersModal({ campaignName, campaignId }: InvitePlayersModalProps) {
+
+  const { setShowModal, setModalBody } = useModalContext();
+  const [searchVal, setSearchVal] = useState("");
+  const [foundPersons, setFoundPersons] = useState<Array<IPerson>>([])
+
+  const handleInputChange: ChangeEventHandler<HTMLInputElement> = 
+    async (e: ChangeEvent<HTMLInputElement>) => {
+      e.preventDefault();
+      setSearchVal(e.target.value);
+    }
+
+  const handleInvitePlayer = (invitee: IPerson) => {
+    setModalBody(
+      <InviteMessageModal 
+        campaignName={campaignName} 
+        campaignId={campaignId} 
+        player={invitee} 
+      />
+    );
+  }
+
+  useEffect(() => {
+    try { 
+      if (searchVal.length == 0) return;
+      // else
+      const response = fetch("/api/people?" + new URLSearchParams({
+        "search": searchVal
+      }));
+      response.then(res => res.json())
+      .then(json => {
+        console.log(json);
+        setFoundPersons(json);
+      })
+    } catch (err) {
+      console.log(err);
+    }
+  }, [searchVal]);
+
+  return (
+    <form>
+      <label>Search by username or email</label>
+      <input 
+        type="text" 
+        onChange={handleInputChange} 
+        placeholder="Username or email" 
+        value={searchVal}
+      />
+      {foundPersons.map(fp => {
+        console.log(fp);
+        return (
+          <div 
+            className="cursor-pointer hover:bg-slate-300"
+            onClick={(e) => {
+              e.preventDefault();
+              handleInvitePlayer(fp)
+            }}
+            
+          >
+            <div className="inline-block p-2">
+              <img className="h-6 w-6" src={fp.imageUrl} />
+            </div>
+            <div className="inline-block p-2">
+              <p>{fp.username}</p>
+              <p>{fp.email}</p>
+            </div>
+          </div>
+        )
+      })}
+    </form>
+  )
+}
+
+
 type CampaignProps = {
   initCampaign: ICampaign
 }
@@ -13,80 +136,13 @@ type CampaignProps = {
 export default function Campaign({initCampaign} : CampaignProps) {
 
   const { setShowModal, setModalBody } = useModalContext();
-
   const [campaign, setCampaign] = useState<ICampaign>(initCampaign);
 
-
-  
-  
-
-
-
-  
-
-  function InvitePlayersModal() {
-    const [searchVal, setSearchVal] = useState("");
-    const [foundPersons, setFoundPersons] = useState<Array<IPerson>>([])
-
-    const handleInputChange: ChangeEventHandler<HTMLInputElement> = 
-      async (e: ChangeEvent<HTMLInputElement>) => {
-        e.preventDefault();
-        setSearchVal(e.target.value);
-      }
-
-    useEffect(() => {
-      try { 
-        if (searchVal.length == 0) return;
-        // else
-        const response = fetch("/api/people?" + new URLSearchParams({
-          "search": searchVal
-        }));
-        response.then(res => res.json())
-        .then(json => {
-          console.log(json);
-          setFoundPersons(json);
-        })
-      } catch (err) {
-        console.log(err);
-      }
-    }, [searchVal]);
-
-    return (
-      <form>
-        <label>Search by username or email</label>
-        <input 
-          type="text" 
-          onChange={handleInputChange} 
-          placeholder="Username or email" 
-          value={searchVal}
-        />
-        {foundPersons.map(fp => {
-          console.log(fp);
-          return (
-            <div>
-              <div className="inline-block p-2">
-                <img className="h-6 w-6" src={fp.imageUrl} />
-              </div>
-              <div className="inline-block p-2">
-                <p>{fp.username}</p>
-                <p>{fp.email}</p>
-              </div>
-            </div>
-          )
-        })}
-      </form>
-    )
-  }
 
   const handleInvitePlayersClick = (e: MouseEvent) => {
     e.preventDefault();
     setShowModal(true);
-    setModalBody(
-      <InvitePlayersModal 
-
-        // handleInputChange={handle}
-      />
-    );
+    setModalBody(<InvitePlayersModal campaignName={campaign.name} campaignId={campaign._id.toString()}/>);
   }
 
   return (
