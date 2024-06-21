01fb2e839fea14332c0948e2a2dd07d595380fe3
Connor Wales <connorwales@gmail.com>
2024-06-20 21:18:11 -0700

    add 'ThreadNotFoundErr' 404 error to NextError

--- lib/NextError.ts
+++ lib/NextError.ts
@@ -18,9 +18,11 @@ export const UsernameTakenErr: NextError = new NextError("A user already exists 
 export const EmailTakenErr: NextError = new NextError("A user already exists with that email", 400);
 export const InvalidMessageErr: NextError = new NextError("Invalid message", 400);
 
+
 // 401
 export const InvalidPasswordErr: NextError = new NextError("Invalid Password", 401);
 
 // 404
 export const UserNotFoundErr: NextError = new NextError("User not found", 404);
 export const CampaignNotFoundErr: NextError = new NextError("Campaign not found", 404);
+export const ThreadNotFoundErr: NextError = new NextError("Cannot find that thread", 404);
