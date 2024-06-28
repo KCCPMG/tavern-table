b77d70764c0fd00b6ec6492e4e0ce7fa72739c38
Connor Wales <connorwales@gmail.com>
2024-06-28 10:22:21 -0700

    update components/ThreadsList.tsx to rely on imageUrl coming from thread itself, images rendering successfully

--- components/ThreadsList.tsx
+++ components/ThreadsList.tsx
@@ -13,45 +13,22 @@ type ThreadsListProps = {
 
 export default function ThreadsList({initThreads, userId}: ThreadsListProps) {
 
-  console.log(initThreads);
+  // console.log(initThreads);
 
   return (
     <ul>
       {initThreads.map( it => {
 
-        const imageUrlArr = []
-
-        if (it.chatType === "CHAT") {
-          const otherParticipant = it.participants.filter(pt => pt._id !== userId)[0];
-          imageUrlArr.push(otherParticipant.imageUrl);
-
-        } else if (it.chatType === "CAMPAIGN") {
-          // get campaign imageUrl
-        } else if (it.chatType === "ROOM") {
-          const otherParticipants = it.participants.filter(pt => pt._id !== userId);
-          otherParticipants.forEach(op => imageUrlArr.push(op.imageUrl));
-        }
-
-        console.log(it.chatType);
-        console.log(imageUrlArr);
-
         return (
           <li className="p-1 border" key={it.threadId}>
             <Link href={`/threads/${it.threadId}`}>
               <div className="relative">
-                {
-                  imageUrlArr.map(url => {
-                    return (
-                      <img 
-                        width="70"
-                        height="70"
-                        className="inline-block m-2 relative top-0"
-                        key={new Date().toDateString()} 
-                        src={url} 
-                      />
-                    );
-                  })
-                }
+                <img 
+                  width="70"
+                  height="70"
+                  className="inline-block m-2 relative top-0"
+                  src={it.imageUrl} 
+                />
                 <div className="inline-block">
                   <h5>{it.name}</h5>
                   {it.messages[0] && 
