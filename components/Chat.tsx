329f6a8f57046a3354aa99023414cff631cbe519
Connor Wales <connorwales@gmail.com>
2024-07-01 12:06:17 -0700

    adjust basic styling on messages to accommodate invitation messages

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -3,6 +3,7 @@
 import { IReactThread } from "@/models/Thread";
 import { IReactMessage } from "@/models/Message";
 import { IPerson } from "@/models/User";
+import { MESSAGE_TYPES } from "@/models/constants";
 
 type IHydratedReactMessage = Omit<IReactMessage, 'sender'> & {
   sender: IPerson | undefined
@@ -16,14 +17,19 @@ type ChatProps = {
 
 export default function Chat({initThread, userId}: ChatProps) {
   
-  const messages: IReactMessage[] | IHydratedReactMessage[] = initThread.messages
+  const messages: IReactMessage[] | IHydratedReactMessage[] = initThread.messages;
+
+  console.log({initThread});
 
   const hydratedMessages = messages.map(msg => {
     return ({...msg, 
-      sender: initThread.participants.find(p => p._id == msg.sender)
+      sender: initThread.participants.find(p => p._id == msg.sender),
+      directRecipient: initThread.participants.find(p => p._id == msg.directRecipient),
     })
 
   })
+
+  console.log({hydratedMessages})
 
   return (
     <>
@@ -31,32 +37,73 @@ export default function Chat({initThread, userId}: ChatProps) {
       <div className="border w-full p-2 content">
         {hydratedMessages.map(hm => {
 
-          const senderIsUser: boolean = hm.sender?._id === userId
+          const senderIsUser: boolean = hm.sender?._id === userId;
+          const messageIsInvitation: boolean = (
+            hm.messageType === MESSAGE_TYPES.CAMPAIGN_INVITE ||
+            hm.messageType === MESSAGE_TYPES.ROOM_INVITE
+          )
+
+          const senderIsUserStyle = senderIsUser ? 
+            "float-right" : "float-left"
+
+          const messageIsInvitationStyle = messageIsInvitation ? 
+            "bg-slate-300" : ""
+
+          const messageClassName = [
+            'border w-5/6',
+            senderIsUserStyle,
+            messageIsInvitationStyle
+          ].join(" ")
+
+
+          function generateInvitationSummaryMessage() {
+            
+            const inviter = (hm.sender!._id === userId) ? 
+              "You" : hm.sender!.username;
+
+            const invitee = (hm.directRecipient?._id === userId) ? 
+              "you" : hm.directRecipient?.username;
+
+            const campaignOrGroup = hm.messageType === MESSAGE_TYPES.CAMPAIGN_INVITE ? "Campaign" : "Group";
+
+            return inviter + " invited " + invitee + " to join the " + campaignOrGroup
+          }
+          // const invitationString = messageIsInvitation ?
+          //   (hm.sender === userId) ? "You" :  :
+          //   ""
 
           return (
+            // <div 
+            //   key={hm._id}
+            //   className={messageClassName}
+            // >
+            //   <div 
+            //     className={
+            //       `border w-auto max-w-full p-1 m-1
+            //       ${senderIsUser ? 
+            //         "float-right" : 
+            //         "float-left"
+            //       }`
+            //     }
+            //   >
+            //     <span>{hm.text}</span>
+            //   </div>
+
+            // </div>
             <div 
               key={hm._id}
-              className={`
-                border
-                w-5/6
-                ${senderIsUser ? 
-                  "float-right" : 
-                  "float-left"
-                }`
-              }
+              className="w-full block"
             >
               <div 
-                className={
-                  `border 
-                  w-auto
-                  max-w-full
-                  p-1
-                  m-1
-                  ${senderIsUser ? 
-                    "float-right" : 
-                    "float-left"
-                  }`
-                }
+                // className={
+                //   `border w-auto max-w-[85%] p-1 m-1
+                //   ${senderIsUser ? 
+                //     "float-right" : 
+                //     "float-left"
+                //   }`
+                // }
+                // className="border w-auto max-w-[85%] p-1 flex justify-end"
+                className={(senderIsUser ? "justify-end ml-16 " : "justify-start mr-16") + " border p-1 flex my-2"}
               >
                 <span>{hm.text}</span>
               </div>
