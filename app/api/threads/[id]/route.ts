4a466e2d28c60fd32ce95f8f82650a97133d1a88
Connor Wales <connorwales@gmail.com>
2024-07-01 10:30:23 -0700

    add api/threads/[id]/route and use it to correct data fetchin in app/threads/[id]/page

---
+++ app/api/threads/[id]/route.ts
@@ -0,0 +1,31 @@
+import { NextRequest } from "next/server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/api/auth/[...nextauth]/route";
+import { MustSignInErr, ThreadAccessDeniedErr } from "@/lib/NextError";
+import handleErrorResponse from "@/lib/handleErrorResponse";
+import Thread from "@/models/Thread";
+
+
+export async function GET(req: NextRequest, {params}: {params: {id: string}}){
+  try {
+
+    const session = await getServerSession(authOptions);
+    const userId = session?.user._id 
+    const threadId = params.id;
+
+    if (!userId) throw MustSignInErr;
+  
+    const thread = await Thread.getThread(threadId, userId);
+
+    if ( !(thread.participants.find(p => p._id ===userId)) ) {
+      throw ThreadAccessDeniedErr;
+    }
+
+    return Response.json(thread);
+
+  } catch(err) {
+    return handleErrorResponse(err);  
+  }
+
+
+}
--- app/threads/[id]/page.tsx
+++ app/threads/[id]/page.tsx
@@ -5,6 +5,9 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/api/auth/[...nextauth]/route";
 import ToastRedirect from "@/components/ToastRedirect";
 import Image from "next/image";
+import { headers } from "next/headers";
+import { GET } from "@/api/threads/[id]/route";
+import { NextRequest, NextResponse } from "next/server";
 
 const placeholderImgString = "/sample.jpg";
 
@@ -16,7 +19,7 @@ type PageProps = {
   }
 }
 
-export default async function Page({params}: PageProps ) {
+export default async function Page({ params }: PageProps  ) {
 
   const session = await getServerSession(authOptions);
   // console.log({"session.user": session?.user})
@@ -32,26 +35,67 @@ export default async function Page({params}: PageProps ) {
     />
   );
 
-  const initThread = await Thread.getThread(params.id, session.user._id);
+  try {
+    // const initThread = await Thread.getThread(params.id, session.user._id);
+    const initThreadReq = await fetch(
+      process.env.NEXTAUTH_URL + `/api/threads/${params.id}`, {
+        headers: headers()
+      }
+    );
+
+    console.log({
+      "request status": initThreadReq.status, 
+      "request status text": initThreadReq.statusText
+    });
+
+    if (!initThreadReq.ok) {
+      throw new Error(initThreadReq.statusText);
+    }
+
+    const initThread: IReactThread = await initThreadReq.json()
+
+    // if (!(initThread.participants.find(p => p._id === session.user._id))) {
+    //   return (
+    //     <ToastRedirect 
+    //       toasts={[
+    //         {
+    //           message: "You do not have permission to view this thread",
+    //           status: "error"
+    //         }
+    //       ]}  
+    //       redirect="/messages"  
+    //     />
+    //   )
+    // }
 
 
-
-  return (
-    <>
-      <div>
-        <Image 
-          src={initThread.imageUrl || placeholderImgString} 
-          alt={initThread.name} 
-          width="100"
-          height="100"
-          className="inline-block"
-        />
-        <div className="inline-block">
-          <h1>{initThread.name}</h1>
+    return (
+      <>
+        <div>
+          <Image 
+            src={initThread.imageUrl || placeholderImgString} 
+            alt={initThread.name} 
+            width="100"
+            height="100"
+            className="inline-block"
+          />
+          <div className="inline-block">
+            <h1>{initThread.name}</h1>
+          </div>
         </div>
-      </div>
-      <Chat initThread={initThread} userId={session.user._id}/>
-    </>
-  )
+        <Chat initThread={initThread} userId={session.user._id}/>
+      </>
+    )
+  } catch (err) {
+    console.log(err);
+    const error = err as Error;
+    return <ToastRedirect redirect="/messages" toasts={[
+      {
+        message: error!.message || "Something went wrong",
+        status: 'error'
+      }
+    ]} />
+  }
+  
 
 };
